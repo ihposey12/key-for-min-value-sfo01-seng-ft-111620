96e1b1771d8b520e805369abb9f0a76413bad9f9
static-tutorial-1700 <ihposey@crimson.ua.edu>
2020-10-31 00:53:33 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,7 +7,7 @@ def key_for_min_value(name_hash)
   name_hash.each do |key, value|
     if low_value == 0 || value < low_value
       low_value = value
-      low+key = key
+      low_key = key
     end
   end
   low_key
